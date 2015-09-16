07fa0bab3eb5bc74b5d06d958e65a0750e792379
Alessandro Maffei <amaffei@intercomsolutions.it>
2015-09-16 09:15:18 +0200

    Test commit

--- src/test/java/com/intercom/web/smtcollector/client/SmtCollectorClientTest.java
+++ src/test/java/com/intercom/web/smtcollector/client/SmtCollectorClientTest.java
@@ -40,7 +40,7 @@ public class SmtCollectorClientTest {
 	private SmtCollectorClient client;
 	private static final String URL = "http://smtcollector.interlaced.it/api";
 	private static final String USERNAME = "a.maffei";
-	private static final String PASSWORD = "int166";
+	private static final String PASSWORD = "PASSWORD";
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	public SmtCollectorClientTest() {
